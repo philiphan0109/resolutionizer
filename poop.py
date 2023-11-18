44196c8bf635c770b466d21a128b58e874d2b9dd
philiphan0109 <philiphan0109@hotmail.com>
2023-11-17 23:24:05 -0500

    poop

---
+++ poop.py
@@ -0,0 +1,13 @@
+import os
+import glob
+import shutil
+
+data_path = "imagenet-object-localization-challenge/ILSVRC/Data/CLS-LOC/train/"
+output_path = "data/"
+
+for sub_folder in next(os.walk(data_path))[1]:
+    path = os.path.join(data_path, sub_folder)
+    files = glob.glob(os.path.join(path, ""))[:10]
+    
+    for file in files:
+        shutil.move(file, "~/data") 
